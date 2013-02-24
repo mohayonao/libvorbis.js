2650b4a85d599c18aeebd67149717ceabf91df8d
mohayonao <mohayonao@gmail.com>
2013-02-24 16:08:22 +0900

    added: _make_words

--- src/sharedbook.js
+++ src/sharedbook.js
@@ -2,7 +2,88 @@
    for length ordered or unordered, always assigns the lowest valued
    codewords first.  Extended to handle unused entries (length 0) */
 function _make_words(l, n, sparsecount) {
-  NOT_IMPLEMENTED();
+  var i,j,count=0;
+  var marker = calloc(33,uint32);
+  var r=calloc((sparsecount?sparsecount:n),uint32);
+  var length,entry,temp;
+  
+  for(i=0;i<n;i++){
+    length=l[i];
+    if(length>0){
+      entry=marker[length];
+      
+      /* when we claim a node for an entry, we also claim the nodes
+         below it (pruning off the imagined tree that may have dangled
+         from it) as well as blocking the use of any nodes directly
+         above for leaves */
+      
+      /* update ourself */
+      if(length<32 && (entry>>>length)){
+        /* error condition; the lengths must specify an overpopulated tree */
+        return(NULL);
+      }
+      r[count++]=entry;
+      
+      /* Look to see if the next shorter marker points to the node
+         above. if so, update it and repeat.  */
+      {
+        for(j=length;j>0;j--){
+          
+          if(marker[j]&1){
+            /* have to jump branches */
+            if(j===1)
+              marker[1]++;
+            else
+              marker[j]=marker[j-1]<<1;
+            break; /* invariant says next upper marker would already
+                      have been moved if it was on the same path */
+          }
+          marker[j]++;
+        }
+      }
+      
+      /* prune the tree; the implicit invariant says all the longer
+         markers were dangling from our just-taken node.  Dangle them
+         from our *new* node. */
+      for(j=length+1;j<33;j++)
+        if((marker[j]>>1) === entry){
+          entry=marker[j];
+          marker[j]=marker[j-1]<<1;
+        }else
+          break;
+    }else
+      if(sparsecount===0)count++;
+  }
+  
+  /* sanity check the huffman tree; an underpopulated tree must be
+     rejected. The only exception is the one-node pseudo-nil tree,
+     which appears to be underpopulated because the tree doesn't
+     really exist; there's only one possible 'codeword' or zero bits,
+     but the above tree-gen code doesn't mark that. */
+  if(sparsecount !== 1){
+    for(i=1;i<33;i++)
+      if(marker[i] & (0xffffffff>>>(32-i))){
+        return(NULL);
+      }
+  }
+  
+  /* bitreverse the words because our bitwise packer/unpacker is LSb
+     endian */
+  for(i=0,count=0;i<n;i++){
+    temp=0;
+    for(j=0;j<l[i];j++){
+      temp<<=1;
+      temp|=(r[count]>>j)&1;
+    }
+    
+    if(sparsecount){
+      if(l[i])
+        r[count++]=temp;
+    }else
+      r[count++]=temp;
+  }
+  
+  return(r);
 }
 
 /* there might be a straightforward one-line way to do the below

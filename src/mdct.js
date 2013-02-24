88151ef46054bd6204dd26a54e9bd97780dd5742
mohayonao <mohayonao@gmail.com>
2013-02-24 15:36:32 +0900

    added: mdct_init

--- src/mdct.js
+++ src/mdct.js
@@ -8,7 +8,45 @@
    some window function algebra. */
 
 function mdct_init(lookup, n) {
-  NOT_IMPLEMENTED();
+  var bitrev=calloc((n/4),int16);
+  var T=calloc((n+n/4),float32);
+  
+  var i,j;
+  var n2=n>>1;
+  var log2n=lookup.log2n=rint(Math.log(n)*Math.LOG2E);
+  var mask,msb,acc;
+  lookup.n=n;
+  lookup.trig=T;
+  lookup.bitrev=bitrev;
+  
+  /* trig lookups... */
+  
+  for(i=0;i<n/4;i++){
+    T[i*2]=FLOAT_CONV(Math.cos((Math.PI/n)*(4*i)));
+    T[i*2+1]=FLOAT_CONV(-Math.sin((Math.PI/n)*(4*i)));
+    T[n2+i*2]=FLOAT_CONV(Math.cos((Math.PI/(2*n))*(2*i+1)));
+    T[n2+i*2+1]=FLOAT_CONV(Math.sin((Math.PI/(2*n))*(2*i+1)));
+  }
+  for(i=0;i<n/8;i++){
+    T[n+i*2]=FLOAT_CONV(Math.cos((Math.PI/n)*(4*i+2))*0.5);
+    T[n+i*2+1]=FLOAT_CONV(-Math.sin((Math.PI/n)*(4*i+2))*0.5);
+  }
+  
+  /* bitreverse lookup... */
+  
+  {
+    mask=(1<<(log2n-1))-1;
+    msb=1<<(log2n-2);
+    for(i=0;i<n/8;i++){
+      acc=0;
+      for(j=0;msb>>j;j++)
+        if((msb>>j)&i)acc|=1<<j;
+      bitrev[i*2]=((~acc)&mask)-1;
+      bitrev[i*2+1]=acc;
+
+    }
+  }
+  lookup.scale=FLOAT_CONV(4.0/n);
 }
 
 /* 8 point butterfly (in place, 4 register) */

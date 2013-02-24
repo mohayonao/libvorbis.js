5a0842469a5d6715fccf58751d89e2deada5fb5b
mohayonao <mohayonao@gmail.com>
2013-02-24 16:01:39 +0900

    added: vorbis_book_init_decode

--- src/sharedbook.js
+++ src/sharedbook.js
@@ -59,7 +59,122 @@ function vorbis_book_init_encode(c, s) {
 
 /* decode codebook arrangement is more heavily optimized than encode */
 function vorbis_book_init_decode(c, s) {
-  NOT_IMPLEMENTED();
+  var i,j,n=0,tabn;
+  var sortindex,codes,codep,position,orig,mask,hi,lo,word,loval,hival;
+  codebook(c);
+  
+  /* count actually used entries */
+  for(i=0;i<s.entries;i++)
+    if(s.lengthlist[i]>0)
+      n++;
+  
+  c.entries=s.entries;
+  c.used_entries=n;
+  c.dim=s.dim;
+  
+  err_out:while(1){
+    if(n>0){
+      
+      /* two different remappings go on here.
+         
+         First, we collapse the likely sparse codebook down only to
+         actually represented values/words.  This collapsing needs to be
+         indexed as map-valueless books are used to encode original entry
+         positions as integers.
+         
+         Second, we reorder all vectors, including the entry index above,
+         by sorted bitreversed codeword to allow treeless decode. */
+      
+      /* perform sort */
+      codes=_make_words(s.lengthlist,s.entries,c.used_entries);
+      codep=calloc(n,[]);
+      
+      if(codes===NULL)break err_out;
+      
+      for(i=0;i<n;i++){
+        codes[i]=bitreverse(codes[i]);
+        codep[i]=pointer(codes,i);
+      }
+      // qsort(codep,n,sizeof(*codep),sort32a);
+      codep.sort(sort32a);
+      
+      sortindex=calloc(n,int16);
+      c.codelist=calloc(n,uint32);
+      /* the index is a reverse index */
+      for(i=0;i<n;i++){
+        position=codep[i]-codes;
+        sortindex[position]=i;
+      }
+      
+      for(i=0;i<n;i++)
+        c.codelist[sortindex[i]]=codes[i];
+      codes=null;
+      
+      c.valuelist=_book_unquantize(s,n,sortindex);
+      c.dec_index=calloc(n,int16);
+      
+      for(n=0,i=0;i<s.entries;i++)
+        if(s.lengthlist[i]>0)
+          c.dec_index[sortindex[n++]]=i;
+      
+      c.dec_codelengths=calloc(n,int16);
+      for(n=0,i=0;i<s.entries;i++)
+        if(s.lengthlist[i]>0)
+          c.dec_codelengths[sortindex[n++]]=s.lengthlist[i];
+      
+      c.dec_firsttablen=ilog(c.used_entries)-4; /* this is magic */
+      if(c.dec_firsttablen<5)c.dec_firsttablen=5;
+      if(c.dec_firsttablen>8)c.dec_firsttablen=8;
+      
+      tabn=1<<c.dec_firsttablen;
+      c.dec_firsttable=calloc(tabn,uint32);
+      c.dec_maxlength=0;
+      
+      for(i=0;i<n;i++){
+        if(c.dec_maxlength<c.dec_codelengths[i])
+          c.dec_maxlength=c.dec_codelengths[i];
+        if(c.dec_codelengths[i]<=c.dec_firsttablen){
+          orig=bitreverse(c.codelist[i]);
+          for(j=0;j<(1<<(c.dec_firsttablen-c.dec_codelengths[i]));j++)
+            c.dec_firsttable[orig|(j<<c.dec_codelengths[i])]=i+1;
+        }
+      }
+      
+      /* now fill in 'unused' entries in the firsttable with hi/lo search
+         hints for the non-direct-hits */
+      {
+        mask=0xfffffffe<<(31-c.dec_firsttablen);
+        lo=0;hi=0;
+        
+        for(i=0;i<tabn;i++){
+          word=i<<(32-c.dec_firsttablen);
+          if(c.dec_firsttable[bitreverse(word)]===0){
+            while((lo+1)<n && c.codelist[lo+1]<=word)lo++;
+            while(    hi<n && word>=(c.codelist[hi]&mask))hi++;
+            
+            /* we only actually have 15 bits per hint to play with here.
+               In order to overflow gracefully (nothing breaks, efficiency
+               just drops), encode as the difference from the extremes. */
+            {
+              loval=lo;
+              hival=n-hi;
+              
+              if(loval>0x7fff)loval=0x7fff;
+              if(hival>0x7fff)hival=0x7fff;
+              c.dec_firsttable[bitreverse(word)]=
+                0x80000000 | (loval<<15) | hival;
+            }
+          }
+        }
+      }
+    }
+    
+    return(0);
+  }
+  
+  // err_out:
+  vorbis_book_clear(c);
+  return(-1);
 }
 
 function vorbis_book_codeword(book, entry) {

00238c284a73d851b2cc327691f4c991e531087e
mohayonao <mohayonao@gmail.com>
2013-02-24 10:26:34 +0900

    added: exports.js

---
+++ include/exports.js
@@ -0,0 +1,19 @@
+function VorbisInfo() {
+  vorbis_info(this);
+}
+exports.vorbis.Info = VorbisInfo;
+
+function VorbisDspState() {
+  vorbis_dsp_state(this);
+}
+exports.vorbis.DspState = VorbisDspState;
+
+function VorbisComment() {
+  vorbis_comment(this);
+}
+exports.vorbis.Comment = VorbisComment;
+
+function VorbisBlock() {
+  vorbis_block(this);
+}
+exports.vorbis.Block = VorbisBlock;

d937c4cf345391d3d2973601f24104d65712bce5
mohayonao <mohayonao@gmail.com>
2013-02-24 21:41:35 +0900

    added: vorbis_synthesis

--- src/synthesis.js
+++ src/synthesis.js
@@ -1,5 +1,68 @@
 function vorbis_synthesis(vb, op) {
-  NOT_IMPLEMENTED();
+  var vd= vb ? vb.vd : 0;
+  var b= vd ? vd.backend_state : 0;
+  var vi= vd ? vd.vi : 0;
+  var ci= vi ? vi.codec_setup : 0;
+  var opb=vb ? vb.opb : 0;
+  var type,mode,i;
+  
+  if (!vd || !b || !vi || !ci || !opb) {
+    return OV_EBADPACKET;
+  }
+  
+  /* first things first.  Make sure decode is ready */
+  _vorbis_block_ripcord(vb);
+  oggpack_readinit(opb,op.packet,op.bytes);
+  
+  /* Check the packet type */
+  if(oggpack_read(opb,1)!==0){
+    /* Oops.  This is not an audio data packet */
+    return(OV_ENOTAUDIO);
+  }
+  
+  /* read our mode and pre/post windowsize */
+  mode=oggpack_read(opb,b.modebits);
+  if(mode===-1){
+    return(OV_EBADPACKET);
+  }
+  
+  vb.mode=mode;
+  if(!ci.mode_param[mode]){
+    return(OV_EBADPACKET);
+  }
+  
+  vb.W=ci.mode_param[mode].blockflag;
+  if(vb.W){
+    /* this doesn;t get mapped through mode selection as it's used
+       only for window selection */
+    vb.lW=oggpack_read(opb,1);
+    vb.nW=oggpack_read(opb,1);
+    if(vb.nW===-1){
+      return(OV_EBADPACKET);
+    }
+  }else{
+    vb.lW=0;
+    vb.nW=0;
+  }
+  
+  /* more setup */
+  vb.granulepos=op.granulepos;
+  vb.sequence=op.packetno;
+  vb.eofflag=op.e_o_s;
+  
+  /* alloc pcm passback storage */
+  vb.pcmend=ci.blocksizes[vb.W];
+  // vb.pcm=_vorbis_block_alloc(vb,sizeof(*vb.pcm)*vi.channels);
+  // for(i=0;i<vi.channels;i++)
+  //   vb.pcm[i]=_vorbis_block_alloc(vb,vb.pcmend*sizeof(*vb.pcm[i]));
+  vb.pcm=_vorbis_block_alloc(vb,vi.channels);
+  for(i=0;i<vi.channels;i++)
+    vb.pcm[i]=_vorbis_block_alloc(vb,vb.pcmend);
+  
+  /* unpack_header enforces range checking */
+  type=ci.map_type[ci.mode_param[mode].mapping];
+  
+  return(_mapping_P[type].inverse(vb,ci.map_param[ci.mode_param[mode].mapping]));
 }
 
 /* used to track pcm position without actually performing decode.

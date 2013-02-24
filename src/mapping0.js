f52da99c9fdf7c0127258bd68640615dd8a7e247
mohayonao <mohayonao@gmail.com>
2013-02-24 14:35:50 +0900

    added: mapping0_unpack

--- src/mapping0.js
+++ src/mapping0.js
@@ -17,7 +17,59 @@ function mapping0_pack(vi, vm, opb) {
 
 /* also responsible for range checking */
 function mapping0_unpack(vi, opb) {
-  NOT_IMPLEMENTED();
+  var i,b;
+  var info=vorbis_info_mapping0();
+  var ci=vi.codec_setup;
+  var testM,testA;
+  
+  err_out:while(1){
+    b=oggpack_read(opb,1);
+    if(b<0)break err_out;
+    if(b){
+      info.submaps=oggpack_read(opb,4)+1;
+      if(info.submaps<=0)break err_out;
+    }else
+      info.submaps=1;
+    
+    b=oggpack_read(opb,1);
+    if(b<0)break err_out;
+    if(b){
+      info.coupling_steps=oggpack_read(opb,8)+1;
+      if(info.coupling_steps<=0)break err_out;
+      for(i=0;i<info.coupling_steps;i++){
+        testM=info.coupling_mag[i]=oggpack_read(opb,ilog2(vi.channels));
+        testA=info.coupling_ang[i]=oggpack_read(opb,ilog2(vi.channels));
+        
+        if(testM<0 ||
+           testA<0 ||
+           testM===testA ||
+           testM>=vi.channels ||
+           testA>=vi.channels) break err_out;
+      }
+    }
+    
+    if(oggpack_read(opb,2)!==0)break err_out; /* 2,3:reserved */
+    
+    if(info.submaps>1){
+      for(i=0;i<vi.channels;i++){
+        info.chmuxlist[i]=oggpack_read(opb,4);
+        if(info.chmuxlist[i]>=info.submaps || info.chmuxlist[i]<0)break err_out;
+      }
+    }
+    for(i=0;i<info.submaps;i++){
+      oggpack_read(opb,8); /* time submap unused */
+      info.floorsubmap[i]=oggpack_read(opb,8);
+      if(info.floorsubmap[i]>=ci.floors || info.floorsubmap[i]<0)break err_out;
+      info.residuesubmap[i]=oggpack_read(opb,8);
+      if(info.residuesubmap[i]>=ci.residues || info.residuesubmap[i]<0)break err_out;
+    }
+    
+    return info;
+  }
+  
+  // err_out:
+  mapping0_free_info(info);
+  return(NULL);
 }
 
 function mapping0_forward(vb){
